5fd63a1c5310e7f8304f16483ce9158be56e8cb2
Selimmyrat <you@example.com>
2024-05-22 18:57:05 +0500

    pagination fixes

--- src/components/filterBooks/BooksList.jsx
+++ src/components/filterBooks/BooksList.jsx
@@ -12,81 +12,48 @@ export default function BooksList() {
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("JS");
   const [books, setBooks] = useState([]);
-  const [totalBooks, setTotalBooks] = useState();
+  const [totalBookCount, setTotalBookCount] = useState();
   const [itemOffset, setItemOffset] = useState(0);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${query}&key=AIzaSyCbQRpeKJTIOHfIww1nJ9kfn9JncJgpREQ&maxResults=40&startIndex=${itemOffset}`
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&key=AIzaSyCbQRpeKJTIOHfIww1nJ9kfn9JncJgpREQ&maxResults=${itemsPerPage}&startIndex=${itemOffset}`
     )
       .then((res) => res.json())
       .then((result) => {
         setBooks(result.items);
-        setTotalBooks(result.totalItems);
+        setTotalBookCount(result.totalItems);
         setLoading(false);
       })
       .catch((error) => {
-        alert(error.message);
+        console.log(error.message);
         setLoading(false);
       });
   }, [query, itemOffset]);
 
   const getSearch = (e) => {
     e.preventDefault();
-    setLoading(true);
     if (search !== "") {
       setQuery(search);
     }
   };
 
+  const itemsPerPage = 10;
+
   const handleItemOffset = (offset) => {
     setItemOffset(offset);
   };
 
-  // const booksData = useLoaderData();
+  const handlePageClick = (event) => {
+    const newOffset = event.selected * itemsPerPage;
+    handleItemOffset(newOffset);
+  };
+
   return (
     <div className="ml-5">
       <h1 className="text-3xl mb-10">Books</h1>
-      {/* <div className="w-full flex flex-row justify-between border rounded-xl px-4 py-3">
-        <div className="flex flex-row justify-center items-center">
-          <p className="font-semibold mr-3 text-sm">Today</p>
-          <p className="font-thin mr-3 text-sm">This Week</p>
-          <p className="font-thin text-sm">This Month</p>
-        </div>
-        <div className="flex flex-row">
-          <div className="relative" id="dropdownButtonFilter">
-            <div className="flex flex-row items-center cursor-pointer">
-              <IconSortDescending stroke={2} />
-              <form>
-                <select
-                  id="countries"
-                  className="block bg-transparent border-none outline-none focus:border-none"
-                >
-                  <option value="newest">Newest</option>
-                  <option value="oldest">Oldest</option>
-                  <option value="rating">Rating</option>
-                </select>
-              </form>
-            </div>
-            <div
-              id="dropdownFilter"
-              className="border-[1px] border-gray-300 bg-white rounded-xl top-[20px] absolute w-[90px] text-center hidden"
-            >
-              <div className="cursor-pointer text-sm hover:bg-gray-300 hover:rounded-xl pt-2">
-                Oldest
-              </div>
-              <div className="cursor-pointer text-sm hover:bg-gray-300 hover:rounded-xl py-1">
-                Oldest
-              </div>
-              <div className="cursor-pointer text-sm hover:bg-gray-300 hover:rounded-xl pb-1">
-                Oldest
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
-
       <div className="border rounded-xl p-2 mt-3">
         <form onSubmit={getSearch}>
           <div className="flex flex-row ">
@@ -136,9 +103,11 @@ export default function BooksList() {
       ) : (
         <Pagination
           itemOffset={itemOffset}
-          handleOffset={handleItemOffset}
-          total={totalBooks}
+          // handleOffset={handleItemOffset}
+          totalCountBook={totalBookCount}
           data={books}
+          itemsPerPage={itemsPerPage}
+          handlePageClick={handlePageClick}
         />
       )}
     </div>
--- src/components/filterBooks/pagination.jsx
+++ src/components/filterBooks/pagination.jsx
@@ -4,24 +4,25 @@ import { Link } from "react-router-dom";
 
 import coverNotFound from "../../assets/images/coverNotFound.jpg";
 
-export default function Pagination({ data, total, itemOffset, handleOffset }) {
+export default function Pagination({
+  data,
+  totalCountBook,
+  itemOffset,
+  itemsPerPage,
+  handlePageClick
+}) {
   const [currentItems, setCurrentItems] = useState([]);
   const [pageCount, setPageCount] = useState(0);
-  const [loading, setLoading] = useState(false);
-
-  const itemsPerPage = 30;
 
   useEffect(() => {
-    const endOffset = itemOffset + itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
     setCurrentItems(data);
-    setPageCount(Math.ceil(total / itemsPerPage));
-  }, [itemOffset, itemsPerPage, data, total]);
-  
-  const handlePageClick = (event) => {
-    const newOffset = event.selected * itemsPerPage;
-    handleOffset(newOffset);
-  };
+    setPageCount(Math.ceil(totalCountBook / itemsPerPage));
+  }, [itemsPerPage, data, totalCountBook]);
+
+  // const handlePageClick = (event) => {
+  //   const newOffset = event.selected * itemsPerPage;
+  //   handleOffset(newOffset);
+  // };
 
   return (
     <>
@@ -56,55 +57,23 @@ export default function Pagination({ data, total, itemOffset, handleOffset }) {
           );
         })}
       </div>
-      <p>{`Total books is: ${total}`}</p>
+      <p>{`Total books is: ${totalCountBook}`}</p>
       <div className="flex flex-row justify-between mt-3">
         <p>{`Showing ${itemOffset} from  ${itemsPerPage + itemOffset}`}</p>
-        {loading ? (
-          <div className="flex justify-center items-center bg-white mt-10">
-            <button
-              type="button"
-              className="flex flex-row justify-center items-center"
-              disabled
-            >
-              <svg
-                className="animate-spin -ml-1 mr-3 h-10  w-10 text-purple-600"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-              Loading...
-            </button>
-          </div>
-        ) : (
-          <ReactPaginate
-            breakLabel="..."
-            nextLabel=">"
-            onPageChange={handlePageClick}
-            pageRangeDisplayed={2}
-            pageCount={pageCount}
-            previousLabel="<"
-            renderOnZeroPageCount={null}
-            containerClassName="flex flex-row"
-            pageLinkClassName="hover:bg-purple-600 text-purple-600 font-semibold hover:text-white rounded-md px-3 py-1 mx-1"
-            activeLinkClassName="bg-purple-600 text-white hover:bg-purple-600 font-semibold hover:text-white rounded-md px-3 py-1"
-            previousLinkClassName="bg-purple-100 text-purple-600 hover:bg-purple-600 hover:text-white rounded-md px-3 py-1 mr-2"
-            nextLinkClassName="bg-purple-100 text-purple-600 hover:bg-purple-600 hover:text-white rounded-md px-3 py-1 mr-2"
-          />
-        )}
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={2}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+          containerClassName="flex flex-row"
+          pageLinkClassName="hover:bg-purple-600 text-purple-600 font-semibold hover:text-white rounded-md px-3 py-1 mx-1"
+          activeLinkClassName="bg-purple-600 text-white hover:bg-purple-600 font-semibold hover:text-white rounded-md px-3 py-1"
+          previousLinkClassName="bg-purple-100 text-purple-600 hover:bg-purple-600 hover:text-white rounded-md px-3 py-1 mr-2"
+          nextLinkClassName="bg-purple-100 text-purple-600 hover:bg-purple-600 hover:text-white rounded-md px-3 py-1 mr-2"
+        />
       </div>
     </>
   );
